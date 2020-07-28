6fed3f494d22e43c805729a0edff9b02f55b8b5d
Ryan Duff <ryanduff613@gmail.com>
2020-07-27 19:42:41 -0700

    add description column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_000024) do
+ActiveRecord::Schema.define(version: 2020_07_28_024019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_000024) do
     t.boolean "is_admin", default: true
     t.string "city"
     t.string "state"
+    t.string "description"
   end
 
 end

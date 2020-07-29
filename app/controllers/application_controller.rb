1801dccff9807b0e3f49394e39c8f68d7a62b23c
Ryan Duff <ryanduff613@gmail.com>
2020-07-28 20:47:40 -0700

    add authenticate_user and authenticate_admin to application controller. issue with signing in has come up again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # before_action :authenticate_user!, :except => [:landing]
+
   helper_method :current_user
+  helper_method :authenticate_admin
 
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+  def authenticate_user!
+    if !current_user
+      flash[:alert] = "Restricted. Logged-in Users Only"
+      redirect_to '/'
+    end
+  end
+
+  def authenticate_admin
+    if !current_user.admin
+      flash[:alert] = "Restricted. Admin only"
+      redirect_to '/products'
     end
   end
 end
